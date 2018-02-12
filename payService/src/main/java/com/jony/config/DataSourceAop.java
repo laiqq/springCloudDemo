671a1d3f1f8214ada47408c641754e88d14690c9
laiquanqiang <quanqiang.lai@bqjr.cn>
2018-02-12 10:18:44 +0800

    去警告，顺便测试提供推送代码

--- payService/src/main/java/com/jony/config/DataSourceAop.java
+++ payService/src/main/java/com/jony/config/DataSourceAop.java
@@ -1,15 +1,5 @@
 package com.jony.config;
 
-import java.lang.reflect.Modifier;
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.ibatis.javassist.ClassClassPath;
-import org.apache.ibatis.javassist.ClassPool;
-import org.apache.ibatis.javassist.CtClass;
-import org.apache.ibatis.javassist.CtMethod;
-import org.apache.ibatis.javassist.bytecode.CodeAttribute;
-import org.apache.ibatis.javassist.bytecode.LocalVariableAttribute;
-import org.apache.ibatis.javassist.bytecode.MethodInfo;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
@@ -76,35 +66,6 @@ public class DataSourceAop {
         log.info("dataSource == >：read  读库");
     }*/
     
-    /**
-    * @Description 获取字段名和字段值
-    * @Author 刘俊重
-    * @date 2017年7月6日 
-    * @return Map<String,Object>
-    */
-    private Map<String,Object> getFieldsName(Class cls, String clazzName, String methodName, Object[] args) throws Exception {   
-            Map<String,Object > map=new HashMap<String,Object>(); 
-
-
-            ClassPool pool = ClassPool.getDefault();    
-            ClassClassPath classPath = new ClassClassPath(cls);    
-            pool.insertClassPath(classPath);    
-                
-            CtClass cc = pool.get(clazzName);    
-            CtMethod cm = cc.getDeclaredMethod(methodName);    
-            MethodInfo methodInfo = cm.getMethodInfo();  
-            CodeAttribute codeAttribute = methodInfo.getCodeAttribute();    
-            LocalVariableAttribute attr = (LocalVariableAttribute) codeAttribute.getAttribute(LocalVariableAttribute.tag);    
-            if (attr == null) {    
-                // exception    
-            }    
-            int pos = Modifier.isStatic(cm.getModifiers()) ? 0 : 1;    
-            for (int i = 0; i < cm.getParameterTypes().length; i++){    
-                map.put( attr.variableName(i + pos),args[i]);//paramNames即参数名    
-            }    
-            return map;    
-        }       
-
 
     @After("execution(* com.jony.service.impl.*.*(..)) ")
     public void afterReturning() throws Throwable {
